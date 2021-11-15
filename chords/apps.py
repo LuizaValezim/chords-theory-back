13d11d6717744b22cba2f3243b79e544b8751690
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-11-15 20:01:54 -0300

    tentando adicionar o heroku

--- chords/apps.py
+++ chords/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class ChordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chords'
+    name = 'chords' 
--- getit/settings.py
+++ getit/settings.py
@@ -29,6 +29,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'getit.urls'
@@ -88,6 +89,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
