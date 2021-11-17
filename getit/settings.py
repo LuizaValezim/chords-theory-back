076b53a8e7ab5b43fd7bb059937a62b0f7effbe7
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-11-17 18:07:27 -0300

    Deploy funcionando

--- getit/settings.py
+++ getit/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-zzv@!l2k*@%=@ta+4gy&in(&7)m_w*xmd#nou(4n3r7gcc3_!h
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['frozen-badlands-04318.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['chords-theory.herokuapp.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'corsheaders',
